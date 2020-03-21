1bf8ac8db9b1b9117f200f39eaa66f4fb1a80af4
Fabio Romeiro <flromeiroc@gmail.com>
2020-03-21 15:41:00 -0300

    Landing page connectary

---
+++ src/components/Logo/Logo.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="logo" :class="`logo--${ variant }`">
+        <img class="logo__symbol" src="@/assets/images/logo_symbol.svg" alt="Logo do Connectary">
+        <h1 class="logo__title">
+            <span class="blue">Connect</span>
+            <span class="pink">ary</span>
+        </h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Logo',
+    props: {
+        variant: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .logo {
+        display: inline-flex;
+        align-items: center;
+        color: white;
+
+        &__symbol {
+            width: 50px;
+            margin-right: spacing(1);
+        }
+
+        &__title {
+            font-size: 32px;
+            margin-bottom: 4px;
+
+            .blue {
+                color: $color-blue;
+                font-weight: $font-weight-medium;
+            }
+
+            .pink {
+                color: $color-pink;
+                font-weight: $font-weight-light;
+            }
+        }
+
+        &--blue {
+            color: $color-blue;
+        }
+    }
+</style>
